6b265775694e2d844370464131bf9741c9cd5929
DariuszPelka <dariusz.pelka@almservices.pl>
2018-12-01 15:59:28 +0100

    Dodanie GitRepository project

---
+++ GitRepository/IGitRepositoryAccess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitRepository
+{
+    public interface IGitRepositoryAccess
+    {
+        int GetNumberOfCommitsForUserOnDate(DateTime date, int userId);
+
+        int GetAverageNumberOfCommitsForUserOnDate(int userId);
+    }
+}
---
+++ GitRepository/Model/GitCommit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitRepository.Model
+{
+    public class GitCommit
+    {
+        public string UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public string CommitId { get; set; }
+    }
+}
